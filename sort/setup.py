3e4edce25a64dd3f68f8bcfaec833f7c0cafbaab
AndrewCheUA <107930151+AndrewCheUA@users.noreply.github.com>
2022-09-18 22:57:12 +0300

    Update setup.py

--- sort/setup.py
+++ sort/setup.py
@@ -7,5 +7,5 @@ setup(name='sort',
       author_email='andreicheban@gmail.com',
       url='https://github.com/AndrewCheUA/homework/blob/4f60adf764bf433c87f254faccae8e5e5b485d8f/sort.py',
       packages=['sort'],
-      entry_points={'console_scripts': ['clean-folder = sort.main:sort_main_folder'],},
+      entry_points={'console_scripts': ['clean-folder = sort.sort:sort_main_folder'],},
      )
