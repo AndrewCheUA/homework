8ffe0c14ab0fdb5f6b20d2c320f3a62ca3d7a1fa
AndrewCheUA <107930151+AndrewCheUA@users.noreply.github.com>
2022-09-18 19:44:47 +0300

    Create setup.py

---
+++ sort/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sort',
+      version='1.0',
+      description='Folder sort by files types',
+      author='Andrii Cheban',
+      author_email='andreicheban@gmail.com',
+      url='https://github.com/AndrewCheUA/homework/blob/4f60adf764bf433c87f254faccae8e5e5b485d8f/sort.py',
+      packages=['sort'],
+      entry_points={'console_scripts': ['clean-folder = sort.main:sort_main_folder'],},
+     )
